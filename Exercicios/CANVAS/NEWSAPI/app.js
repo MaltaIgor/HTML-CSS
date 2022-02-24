a46a53b3bab29d9e9e95b6cedcc0c1f024c1ea22
Igor Malta <igorbrunomalta@gmail.com>
2022-02-24 09:01:38 -0300

    update

--- Exercicios/CANVAS/NEWSAPI/app.js
+++ Exercicios/CANVAS/NEWSAPI/app.js
@@ -6,7 +6,22 @@ function exibeNoticias () {
 
     //Montar texto HTML das notícias
     let dados = JSON.parse(this.responseText)
+    for(i=0; i<dados.articles.length; i++){
+        let noticia = dados.articles[i]
+        let data = new Date(noticia.publishedAt) 
 
+        texto = texto + `
+                <div class="box-noticia">
+                <img src="${noticia.urlToImage}" alt=""><br>
+                <span class="titulo">${noticia.title}</span><br>
+                <span class="creditos">${data.toLocaleDateString()} - ${noticia.source.name} - ${noticia.author}</span><br>
+                <span class="text">
+                    ${noticia.content}
+                    <a href="${noticia.url}">Leia mais...</a>
+                </span>
+            </div>
+        `
+    }
 
 
     //Preencher a DIV com o texto HTML
